1cca514caca94e9a0f3b1133d7a7009ef8c1457b
David Lukac <1215290+davidlukac@users.noreply.github.com>
2021-06-23 01:00:05 +0200

    Add a stub module for creating an AWS EKS cluster; Clean up variables

--- environments/litecoin/variables.tf
+++ environments/litecoin/variables.tf
@@ -7,45 +7,6 @@ variable "environment" {
   default = "litecoin"
 }
 
-variable "aws-ecr-repositories" {
-  type = set(string)
-
-  default = [
-    "litecoin",
-    "litecoin-tf"
-  ]
-}
-
-variable "aws-technical-users" {
-  type = set(string)
-
-  default = [
-    "ci",
-    "terraform",
-  ]
-}
-
-variable "aws-technical-groups" {
-  type = set(string)
-
-  default = [
-    "ci",
-    "provisioner"
-  ]
-}
-
-variable "aws-technical-group-membership" {
-  default = {
-    "ci": ["ci"]
-    "terraform": ["provisioner"]
-  }
-}
-
-variable "aws-technical-group-policy" {
-  default = {
-    "ci": ["ECRWriteImages"]
-    // This is very wide access, a custom policy should be create for provisioners like Terraform, but serves a purpose
-    // for now.
-    "provisioner": ["AdministratorAccess"]
-  }
+variable "aws-region" {
+  default = "eu-central-1"
 }
---
+++ modules/aws-eks/variables.tf
@@ -0,0 +1,9 @@
+// Cluster name.
+variable "name" {
+  type = string
+}
+
+// Cluster region.
+variable "region" {
+  type = string
+}
